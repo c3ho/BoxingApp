0ce4b7d4634fc3e98069fa05c6262155aabd660a
Glitch (adorable-python) <none>
2019-07-11 20:23:27 +0000

    💠🤑 Updated with Glitch

---
+++ data-service.js
@@ -0,0 +1,140 @@
+const mongoose = require('mongoose')
+
+
+module.exports = function(mongoURI){
+  
+  let Member;
+  return{
+  connect: function(){
+    return new Promise(function(resolve, reject){
+      let db = mongoose.connect(mongoURI);
+      db.on('error', (err)=>{
+        reject(err);
+      });
+      
+      const Schema = mongoose.Schema;
+      var memberSchema = new Schema({
+        firstName: {
+          type: String,
+          required: true
+        },
+        lastName: {
+          type: String,
+          required: true
+        },
+        enrollDate: {
+          type: Date,
+          default: Date.now(),
+          required: true
+        },
+        expireDate: {
+          type: Date,
+        },
+        lastPaidDate: Date,
+        membershipType: {
+          type: String,
+          default: 'Trial',
+          enum: ['Monthly', 'Pack', 'Trial']
+        },
+        lastActiveDate: [Date],
+        medical: Date,
+        active: Boolean
+      });
+      
+      db.once('open', ()=>{
+        Member = db.model("Member", memberSchema);
+        resolve();
+      });
+    });
+  },
+  renewMembership: function(firstName, lastName, months){
+    Member.find({name: firstName, lastName: lastName}, function (err,data){
+    if (data){
+      data.expireDate = data.expireDate.setMonth(data.expireDate.getMonth() + months)
+      data.save(function(err){
+        if (err)
+          console.log("Error: " + err);
+        else
+          console.log("Success! " + firstName + " " + lastName + " has been renewed for " + months);
+      }) 
+    }
+    if (err)
+      console.log("Error: " + err);
+    else
+      console.log("No record with " + firstName + " " + lastName + " was found.");
+  });
+},
+
+//update membership with either 1,3,6,12 months
+renewMembership: function(firstName, lastName, months){
+  Member.find({name: firstName, lastName: lastName}, function (err,data){
+    if (data){
+      data.expireDate = data.expireDate.setMonth(data.expireDate.getMonth() + months)
+      data.save(function(err){
+        if (err)
+          console.log("Error: " + err);
+        else
+          console.log("Success! " + firstName + " " + lastName + " has been renewed for " + months);
+      }) 
+    }
+    if (err)
+      console.log("Error: " + err);
+    else
+      console.log("No record with " + firstName + " " + lastName + " was found.");
+  });
+},
+
+enrollMember: function(firstName, lastName){
+  var m1 = new Member({
+    firstName: firstName,
+    lastName: lastName
+  });
+  m1.save(function (err){
+    if (err)
+      console.log("An isssue occured while trying to save usesr");
+    else
+      console.log(firstName + " " + lastName + " successfully enrolled to Gideon!");
+  })
+},
+
+//returns all members
+findAll: function(){
+  Member.find({}, function(err, members){
+    
+  })
+},
+
+//placeholder for other functions such as returning or filtering list to user
+findMember: function(firstName, lastName){
+  Member.find({
+    firstName: firstName,
+    lastName: lastName
+  }, function(err, member){
+    if (err)
+      console.log("Error: " + err);
+    if (member){
+      console.log("User found");
+    }
+  })
+}
+}
+}
+
+//test member using me
+/*var m1 = new Member({
+    firstName: 'Calvin',
+    lastName: 'Ho',
+    enrollDate: '2016-08-01',
+    lastPaidDate: '2019-07-01',
+    membershipType: 'Monthly'
+    lastActiveDate: [Date]
+    medical: '2018-12-01',
+    active: true
+});
+
+m1.save(function (err){
+  if (err)
+    console.log("An issue occured while trying to save user");
+  else
+    console.log("Success");
+})*/
--- server.js
+++ server.js
@@ -2,20 +2,29 @@
 // where your node app starts
 
 // init project
+const MONGO_URI = 'mongodb+srv://test:tester123@cluster0-l5exq.mongodb.net/test?retryWrites=true&w=majority'
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const dataService = require("./data-service.js");
+const data = dataService(MONGO_URI);
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+//returns all members
+app.get('/members', function(req, res){
+  res.send(findMembers())
+})
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
